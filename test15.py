090bcb252447ac74960bac741a964efc16fef373
rinnkylesefni <lowbudgetkairi@gmail.com>
2024-04-29 11:47:45 +0700

    update

--- test15.py
+++ test15.py
@@ -1,9 +1,13 @@
              #0          1       2          3           4          5
-mybasket = ['apple', 'orange', 'banana' , 'grapes ' , 'mango' , 'prach' ]
-print(mybasket)
-print(type(mybasket))
+mybasket = ['apple', 'orange', 'banana' , 'grapes ' , 'mango' , 'peach' ]
 
-print(mybasket[0])
-print(mybasket[1:5])
-print(mybasket[2:])
+#print(mybasket)
+#print(type(mybasket))
+
+# print(mybasket[0])
+# print(mybasket[1:5])
+# print(mybasket[2:])
 print(mybasket[:5])
+
+if 'peach' in mybasket:
+    print("YES it is")
