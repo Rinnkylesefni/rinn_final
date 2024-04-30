7dbf0289a87ab7eb74080620de7be2d6db9d32e8
rinnkylesefni <lowbudgetkairi@gmail.com>
2024-04-30 12:06:06 +0700

    update

---
+++ test18.py
@@ -0,0 +1,12 @@
+#creare def
+def add(x , y):
+    answer= x + y
+    print(answer)
+
+def sub(x, y):
+    answer = x - y
+    print(answer)
+
+def multiply(x, y):
+    answer = x * y
+    print(answer)
---
+++ test19.py
@@ -0,0 +1,6 @@
+from test18 import *
+#from test21 impoprt add, sub, multiply
+
+add(4,5)
+sub(5,4)
+multiply(3,4)
