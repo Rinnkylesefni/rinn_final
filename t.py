b05a8f2871afc35f3c5ead6f73d3704407b3347e
rinnkylesefni <lowbudgetkairi@gmail.com>
2024-04-30 11:44:52 +0700

    30_4_24

--- t.py
+++ t.py
@@ -1,2 +1,16 @@
-print("hello world")
-print("hello world")
+# print("hello world")
+# print("hello world")
+
+# mybag = ['1 ' , 'ruler' 'pen' , 'laptop' , 'marker']
+
+# print(mybag)
+
+fav_player ={
+  'name' : '1rrad' ,
+  'role' : 'jungler' , 
+}
+
+fav_player["Signature_hero"] = "Lancelot"
+print(fav_player)
+
+
---
+++ test17.py
@@ -0,0 +1,17 @@
+userinfo ={
+    'Name'  :'R' ,
+    'height':"5 8",
+    'fav_foods' : {
+        'food1' : 'pizza',
+        'food2' : 'cola' ,
+        'food3' : 'tofu' ,
+},
+'fav_songs' : {
+    'song1' : 'strong',
+    'song2' : 'rewrite the star' ,
+    'song3' : 'always'}
+}
+
+# print(userinfo['fav_songs']['song1'])
+# print(userinfo['fav_foods']['food3'])
+# print(userinfo['Name'])
