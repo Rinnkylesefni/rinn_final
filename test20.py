55042d9c58c9891113a2b5eca5214e9e681f8770
rinnkylesefni <lowbudgetkairi@gmail.com>
2024-04-30 12:26:17 +0700

    update

---
+++ test20.py
@@ -0,0 +1,4 @@
+def user_info(name, mail, addr ="Maesot"):
+    print("your name is", name)
+    print("Your mail is", mail)
+    print("Your addr is", addr)
---
+++ test21.py
@@ -0,0 +1,10 @@
+from test20 import user_info
+
+#positional
+user_info("Rinn", "rinnkylesefni@gmail.com", "tokyo")
+
+#keyword
+user_info(addr="Tokyo", name="Rinn",mail="rinnkylesefni@gmail.com")
+
+#default
+user_info("Rinn", "rinnkylesefni@gmail.com")
